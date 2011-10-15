9dab12ed79e7d6049d1ef02cdeb8ee8ad0ebe4cf
uenf-master <github@uenf.de>
2011-10-15 22:12:25 +0200

    first header

---
+++ uenf/Matrix.h
@@ -0,0 +1,24 @@
+#ifndef UENF_MATRIX_H
+#define UENF_MATRIX_H
+
+
+#include <Eigen/Core>
+
+
+namespace uenf
+{
+
+  using namespace Eigen;
+
+  typedef Eigen::Vector2i Vector2i;
+  typedef Eigen::Vector2f Vector2f;
+  typedef Eigen::Vector3f Vector3f;
+
+
+}
+
+
+
+#endif
+
+
